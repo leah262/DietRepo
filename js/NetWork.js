7fe3c45041051c32e20dbfed2c1ce07bc1bce7af
Leah Rov <leahrov2@gmail.com>
2025-06-26 00:10:25 +0300

    networksendmassege

--- js/NetWork.js
+++ js/NetWork.js
@@ -1,16 +1,26 @@
 class NetWork {
     constructor() {
-        this.peopleServer = new PeopleServer();
-        this.filmsServer = new FilmsServer();
+        this.usersServer = new InfoServer();
+        this.infoServer = new FilmsServer();
     }
 
-    sendRequest(url, data) {
-        if (url.startsWith('/people')) {
-            return this.peopleServer.handleRequest(url, data);
-        } else if (url.startsWith('/films')) {
-            return this.filmsServer.handleRequest(url, data);
-        } else {
-            return `Network: No server found for path ${url}`;
+    sendRequest(url, data, callback) {
+        let response;
+        url = new URL(url);
+        if (url.protocol !== "http:" && url.protocol !== "https:") {
+            response = "Invalid protocol";
+            callback(response);
+            return;
         }
+        let pathParts = url.pathname.split('/').filter(Boolean);
+        if (pathParts[1] === "Users-Servers") {
+            response = this.usersServer.handleRequest(url, data);
+        } else if (pathParts[1] === "Info-Server") {
+            response = this.infoServer.handleRequest(url, data);
+        } else {
+            response = `Network: No server found for path ${url.pathname}`;
+        }
+        callback(response);
     }
+
 }
