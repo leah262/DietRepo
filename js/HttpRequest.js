fca778085fb6087caf283e4e15c6b40fb1889148
Dvora <dvoras127@gmail.com>
2025-06-26 18:20:27 +0300

    ברוך מחיה המתים

--- js/HttpRequest.js
+++ js/HttpRequest.js
@@ -1,9 +1,8 @@
-class HttpRequest{
-    constructor(method, section){
-        this.method=method;
-        this.section=new URL(section);
-        console.log("new request");
-        
+class HttpRequest {
+    constructor(method, section) {
+        this.method = method;
+        this.section = new URL(section, "https://fake.server"); // פתרון כאן
+        console.log("new request:", this.method, this.section.href);
     }
 }
 export default HttpRequest;
