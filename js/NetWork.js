84bb12f8b978030c3a8e8af339569434a61e46a4
Dvora <dvoras127@gmail.com>
2025-06-26 17:36:44 +0300

    network

--- js/NetWork.js
+++ js/NetWork.js
@@ -1,35 +1,34 @@
-import  UserServer from "./UserServer.js";
+import UserServer from "./UserServer.js";
 // import  InfoServer from "./infoServer.js";
 
 class NetWork {
     constructor() {
-        this.usersServer =  UserServer;
+        this.userServer = new UserServer();
+
         // this.infoServer = new FilmsServer();
     }
 
-    sendRequest(request,callback) {
-        console.log("in network");
-        console.log(request);
-        
-        let response;
-        url = request.section;
-        if (url.protocol !== "http:" && url.protocol !== "https:") {
-            response = "Invalid protocol";
-            callback(response);
-            return;
-        }
-        let pathParts = url.pathname.split('/').filter(Boolean);
-        if (pathParts[1] === "Users-Servers") {
-            response = this.usersServer.handleRequest(request);
-            console.log("go to user server");
-            
-        } else if (pathParts[1] === "Info-Server") {
-            response = this.infoServer.handleRequest(url, data);
-        } else {
-            response = `Network: No server found for path ${url.pathname}`;
-        }
-        callback(response);
+    sendRequest(httpRequest, callback) {
+    let response;
+    const url = new URL(httpRequest.section); // נשתמש ב-section מה-HttpRequest
+    
+    if (url.protocol !== "http:" && url.protocol !== "https:") {
+        response = "Invalid protocol";
+        if (callback) callback(response);
+        return;
     }
+    
+    let pathParts = url.pathname.split('/').filter(Boolean);
+    if (pathParts[1] === "Users-Servers") {
+        response = this.userServer.handleRequest(url, null);
+    } else if (pathParts[1] === "Info-Server") {
+        response = this.infoServer.handleRequest(url, null);
+    } else {
+        response = `Network: No server found for path ${url.pathname}`;
+    }
+    
+    if (callback) callback(response);
+}
 
 }
 export default new NetWork();
